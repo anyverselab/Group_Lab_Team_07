b65243db017e96ee3d31da37373f2576919b7e56
Any Chung <anychung@Anys-MacBook-Air.local>
2025-10-15 15:38:06 -0400

    Created Member1

--- GroupLab1/src/grouplab1/Main.java
+++ GroupLab1/src/grouplab1/Main.java
@@ -15,7 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-//        Member1.main(args);
+          Member1.main(args);
 //        Member2.main(args);
 //        Member3.main(args);
 //        Member4.main(args);
---
+++ GroupLab1/src/grouplab1/Member1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grouplab1;
+
+/**
+ *
+ * @author anychung
+ */
+public class Member1 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Member 1!");
+    }
+}
