fec5d135f4fe0b8f29a12f23b16b90f7e4906715
Any Chung <anychung@Anys-MacBook-Air.local>
2025-10-15 15:34:14 -0400

    Created Main.java

---
+++ GroupLab1/src/grouplab1/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grouplab1;
+
+/**
+ *
+ * @author Yi-chen Chung
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Member1.main(args);
+//        Member2.main(args);
+//        Member3.main(args);
+//        Member4.main(args);
+    }
+}
